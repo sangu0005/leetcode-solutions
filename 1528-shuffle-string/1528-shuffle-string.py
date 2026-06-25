fec3c210a71f93d6511c6c3a7529ac5b478071a1
Sangamesh Hallur <sshallur2003@gmail.com>
2026-06-25 22:31:10 +0530

    Time: 0 ms (100%), Space: 19.4 MB (21.2%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.py
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -1,12 +1,13 @@
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        res = ""
         hashmap = {}
 
         for i in range(len(s)):
             hashmap[indices[i]] = s[i]
 
-        for i in range(len(s)):
-            res += hashmap[i] 
+        res = []
 
-        return res
+        for i in range(len(s)):
+            res.append(hashmap[i]) 
+
+        return "".join(res)
