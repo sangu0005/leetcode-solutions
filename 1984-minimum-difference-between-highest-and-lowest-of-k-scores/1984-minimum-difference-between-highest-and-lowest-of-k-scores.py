d6a5a8b0b37075d7a1eeb46484263d7ec2d85e3f
Sangamesh Hallur <sshallur2003@gmail.com>
2026-07-01 20:48:41 +0530

    Time: 7 ms (67.95%), Space: 19.3 MB (46.44%) - LeetHub

--- 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ 1984-minimum-difference-between-highest-and-lowest-of-k-scores/1984-minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -5,7 +5,7 @@ class Solution:
 
         nums.sort()
         res = float("inf")
-        for i in range(len(nums) - k +1):
+        for i in range(len(nums) - k + 1):
             res = min(res, nums[i+k-1] - nums[i])
         
         return res
