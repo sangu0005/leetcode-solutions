2a71f40e2ffc2c68c23397f216748235a7a45970
Sangamesh Hallur <sshallur2003@gmail.com>
2026-07-03 19:44:27 +0530

    Time: 7 ms (49.98%), Space: 19.3 MB (58.11%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,8 +1,24 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        row = 1
-        while row <= n:
-            n -= row
-            row += 1
+        # row = 1
+        # while row <= n:
+        #     n -= row
+        #     row += 1
         
-        return row - 1
+        # return row - 1
+
+        l, r = 0, n
+
+        while l <= r:
+            mid = (l + r) // 2
+            coins = mid * (mid + 1) // 2 # coins need for mid row
+            
+            if coins == n:
+                return mid
+            elif coins < n:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return r
+            
