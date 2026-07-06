64d41d638cdc4300ef2e98277a089570ddef7694
Sangamesh Hallur <sshallur2003@gmail.com>
2026-07-06 21:26:10 +0530

    Time: 22 ms (92.56%), Space: 30.9 MB (73.95%) - LeetHub

---
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        seen, res = set(), set()
+
+        for l in range(len(s) - 9):
+            curr = s[l : l + 10]
+
+            if curr in seen:
+                res.add(curr)
+            seen.add(curr)
+        
+        return list(res)
