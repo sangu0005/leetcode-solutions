e917acbc74d7006894550dd65e234190470922ae
Sangamesh Hallur <sshallur2003@gmail.com>
2026-07-17 21:11:43 +0530

    Time: 103 ms (43.3%), Space: 19.6 MB (84.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,7 +6,7 @@ class Solution:
         for r in range(len(s)):
             count[s[r]] = count.get(s[r], 0) + 1
 
-            while (r - l + 1) - max(count.values()) > k:
+            if (r - l + 1) - max(count.values()) > k:
                 count[s[l]] -= 1
                 l += 1
 
