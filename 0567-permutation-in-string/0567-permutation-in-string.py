c87521c29f7f9462dd6a4efcb48e769c06eec96c
Sangamesh Hallur <sshallur2003@gmail.com>
2026-07-27 20:43:35 +0530

    Time: 39 ms (35.86%), Space: 19.5 MB (18.11%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,25 +1,41 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         m, n = len(s1), len(s2)
-
         if m > n:
             return False
 
-        count1 = [0] * 26
-        count2 = [0] * 26
+        need = Counter(s1)
+        window = Counter(s2[ : m])
 
-        for i in range(m):
-            count1[ord(s1[i]) - ord('a')] += 1
-            count2[ord(s2[i]) - ord('a')] += 1
-
-        if count1 == count2:
+        if need == window:
             return True
-        
-        for i in range(m, n):
-            count2[ord(s2[i]) - ord('a')] += 1
-            count2[ord(s2[i - m]) - ord('a')] -= 1
 
-            if count1 == count2:
+        for i in range(m, n):
+            window[s2[i]] += 1
+            window[s2[i - m]] -= 1
+            
+            if need == window:
                 return True
         return False
 
+
+        # m, n = len(s1), len(s2)
+
+        # if m > n:
+        #     return False
+
+        # count1 = [0] * 26
+        # count2 = [0] * 26
+        # for i in range(m):
+        #     count1[ord(s1[i]) - ord('a')] += 1
+        #     count2[ord(s2[i]) - ord('a')] += 1
+        # if count1 == count2:
+        #     return True
+        
+        # for i in range(m, n):
+        #     count2[ord(s2[i]) - ord('a')] += 1
+        #     count2[ord(s2[i - m]) - ord('a')] -= 1
+
+        #     if count1 == count2:
+        #         return True
+        # return False
