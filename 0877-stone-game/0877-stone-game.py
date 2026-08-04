2487aabad76e7052e7a9ea8500920749794f6707
Sangamesh Hallur <sshallur2003@gmail.com>
2026-08-04 19:11:41 +0530

    Time: 0 ms (100%), Space: 19.4 MB (52.3%) - LeetHub

--- 0877-stone-game/0877-stone-game.py
+++ 0877-stone-game/0877-stone-game.py
@@ -1,3 +1,22 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        alice = 0
+        bob = 0
+        l = 0
+        r = len(piles) - 1
+
+        while l <= r:
+            if piles[l] < piles[r]:
+                alice += piles[r]
+                r -= 1
+                if piles[l] > piles[r]:
+                    bob += piles[r]
+                    r -= 1
+                else:
+                    bob += piles[l]
+                    l += 1
+            else:
+                alice += piles[l]
+                l += 1
+        
+        return alice > bob
